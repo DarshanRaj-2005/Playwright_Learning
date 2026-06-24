74efa6783d22ada7531b3c6b0bc3c7375c25fe83
DarshanRaj-2005 <dharshujayapal@gmail.com>
2026-06-24 22:16:23 +0530

    new files added

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,16 +24,24 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
   [
     'html',
     {
-      outputFolder: `playwright-report-${envName}`,
+      outputFolder: 'playwright-report',
       open: 'never'
+    }
+  ],
+  [
+    'allure-playwright',
+    {
+      outputFolder: 'allure-results'
     }
   ]
 ],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -41,8 +49,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    screenshot : 'only-on-failure',
-    video : 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
     // headless : false
   },
 
